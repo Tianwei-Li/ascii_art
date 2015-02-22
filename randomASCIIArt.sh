5943b1ebbae8bfb3ab5fc33795d9ebf748cf2957
Tianwei Li <lee.tianwei@gmail.com>
2015-02-22 15:00:33 -0800

    initial version

---
+++ randomASCIIArt.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+dir=./ascii_art/*
+files=($dir)
+
+numfiles=${#files[@]}
+#echo the file number is $numfiles
+
+fileIdx=$((RANDOM%numfiles))
+
+#echo the file is ${files[fileIdx]}
+cat ${files[fileIdx]}
+
